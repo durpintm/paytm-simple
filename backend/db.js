b4836332abc4dc0ffe129227aa724455903736bc
durpintm <durpinthapa@gmail.com>
2024-05-30 22:13:58 -0400

    Add db connection and routes

---
+++ backend/db.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const uri = "mongodb://localhost:27017/PaytmDB";
+
+mongoose.connect(uri);
+
+const UserSchema = mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  last_name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 25,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+});
+
+const Users = mongoose.model("Users", UserSchema);
+
+export default Users;
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
+import rootRouter from "./routes/router";
 
+const PORT = 3000;
 
+const app = express();
+
+app.use("api/v1", rootRouter);
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Connected...");
+});
