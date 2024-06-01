a24c4499ab990298ab531f1c5f46aea374029640
durpintm <durpinthapa@gmail.com>
2024-06-01 19:49:10 -0400

    User sign up call

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,22 +1,64 @@
+/* eslint-disable no-unused-vars */
 import Heading from "../components/Heading";
 import Button from "../components/Button";
 import BottomWarning from "../components/BottomWarning";
 import InputBox from "../components/InputBox";
 import SubHeading from "../components/SubHeading";
+import { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-[500px] text-center p-2 h-max px-4">
           <Heading label={"Sign Up"} />
           <SubHeading label={"Enter your infromation to create an account"} />
-          <InputBox placeholder="John" label={"First Name"} />
-          <InputBox placeholder="Doe" label={"Last Name"} />
-          <InputBox placeholder="durpinthapa@gmail.com" label={"Email"} />
-          <InputBox placeholder="123456" label={"Password"} />
+          <InputBox
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+            placeholder="John"
+            label={"First Name"}
+          />
+          <InputBox
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+            placeholder="Doe"
+            label={"Last Name"}
+          />
+          <InputBox
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            placeholder="durpinthapa@gmail.com"
+            label={"Email"}
+          />
+          <InputBox
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="123456"
+            label={"Password"}
+          />
           <div className="pt-4">
-            <Button label={"Sign Up"} />
+            <Button
+              onClick={() => {
+                axios.post("http://localhost:3000/api/v1/user/signup", {
+                  username,
+                  first_name: firstName,
+                  last_name: lastName,
+                  password,
+                });
+              }}
+              label={"Sign Up"}
+            />
           </div>
           <BottomWarning
             label={"Already have an account?"}
